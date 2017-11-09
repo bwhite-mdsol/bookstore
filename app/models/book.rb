00edb28667f2baa4e42b96805d1fba8d8f4cf5da
Bob White <bwhite@mdsol.com>
2017-11-09 00:40:28 -0500

    refactor .search

--- app/models/book.rb
+++ app/models/book.rb
@@ -11,7 +11,7 @@ class Book < ApplicationRecord
 
   # return mean rating to one decimal place, nil if no ratings
   def average_rating
-    ratings = book_reviews.pluck(&:rating)
+    ratings = book_reviews.pluck(:rating)
     ratings.size.zero? ? nil : (ratings.sum / ratings.size.to_f).round(1)
   end
 
@@ -25,23 +25,38 @@ class Book < ApplicationRecord
   # search query
   def self.search(query, options = { title_only: false, book_format_type_id: nil, book_format_physical: nil })
     # title query
-    results = Book.where("title LIKE ?", "%#{query.downcase}%")
+    scope = title_search(query)
 
     unless options[:title_only]
       # author query
-      results += Book.where("author_id IN (?)", Author.where("last_name LIKE ?", query.downcase).pluck(:id))
+      scope = scope.or(author_search(query))
+
       # publisher query
-      results += Book.where("publisher_id in (?)", Publisher.where("name LIKE ?", query.downcase).pluck(:id))
+      scope = scope.or(publisher_search(query))
     end
 
-    # results back array to arel
-    results = Book.where(id: results.map(&:id))
+    #results = scope.all
 
     # format filter
-    results = results.with_format(options[:book_format_type_id]) unless options[:book_format_type_id].nil?
-    # physical filter
-    results = results.physical?(options[:book_format_physical]) unless options[:book_format_physical].nil?
+    scope = scope.with_format_id(options[:book_format_type_id]) unless options[:book_format_type_id].nil?
 
-    results.uniq.sort_by(&:average_rating).reverse
+    # physical filter
+    scope = scope.physical?(options[:book_format_physical]) unless options[:book_format_physical].nil?
+
+    # compact, sort and order
+    scope.all.uniq.sort_by(&:average_rating).reverse
+  end
+
+private
+  def self.title_search(query)
+    Book.where("title LIKE ?", "%#{query}%")
+  end
+
+  def self.author_search(query)
+    Book.where("author_id IN (?)", Author.where("last_name LIKE ?", query.downcase).pluck(:id))
+  end
+
+  def self.publisher_search(query)
+    Book.where("publisher_id in (?)", Publisher.where("name LIKE ?", query.downcase).pluck(:id))
   end
 end
